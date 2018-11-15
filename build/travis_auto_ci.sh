3b221d8c7d6e6a90c6a7989305fb2877009e82e2
mdj33 <mdj@disanbo.com>
2018-11-15 10:43:18 +0800

    test pr br

--- build/travis_auto_ci.sh
+++ build/travis_auto_ci.sh
@@ -9,6 +9,16 @@ echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
 echo "TRAVIS_PULL_REQUEST_SHA=$TRAVIS_PULL_REQUEST_SHA"
 echo "TRAVIS_PULL_REQUEST_SLUG=$TRAVIS_PULL_REQUEST_SLUG"
 echo "TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG"
+echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR"
+echo "cur dir"
+pwd
+basepath=$(cd `dirname $0`; pwd)
+echo "base=$basepath"
+echo "git branch"
+git branch
+echo "git status"
+git status
+
 
 
 
