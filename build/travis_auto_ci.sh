fddc3e083e25edc8b7c93aa7667c32ef79adf403
mdj33 <travis@travis-ci.org>
2018-11-15 17:39:29 +0800

    test10

--- build/travis_auto_ci.sh
+++ build/travis_auto_ci.sh
@@ -24,7 +24,7 @@ echo "git status"
 git status
 
 echo "TRAVIS_SECURE_ENV_VARS=$TRAVIS_SECURE_ENV_VARS"
-echo "token=$MDJ33_TOKEN"
+#echo "token=$MDJ33_TOKEN"
 git remote -v
 git branch -r
 
@@ -48,10 +48,10 @@ upload_files() {
 #     local token="aa"
 #    curl -H 'Authorization: token <e32f4a9bcfc918e8e1d4928fa47704d3eb451100>'  https://github.com/mdj33/plugin.git
 #    git remote rm origin
-    local tokenx="edvumnefzvrhdi+wHdE3FkKMghDC0n8IyA6pTU1b2o9COlGFmbDv7A2LNVPagrATAwh9iLWmh6VD7fErHHActOpwi5LkszXci+nM/YsyUgqibdteUCP97dxSILvS3LFI/6bHwWfjPcjYEuykPXLPt9db1i4O3lB4vnQ/nlekb36G+75tfXOXTLDZFgYGLXypQT1mKrnKTqRtmbRq001OQGkypur+mHZmrN4B2gEvbKc2kONdfGvxcp6uOMQyojpgaVDwEw8NKLMDFYWlU6FnYtmc7aiwyDfN8czzv3nicHEs9z2GHvk7l0rD0zE7kj2mxIES90qfc97zbppqxzDlJMMjnUDaQLdhAjyMxrWkjyTeMRRGUNzJqXa+LuiFjWwUlvQMAJttqwWBv832gT6ayCfFtINNwGkECI+lC4IKCt542JdG5ncfyI38Sy1YhMmjVE93trbvUCkd9jy30x3/5Wdqmuq/09gOObdLPrDYiqXPmwgeYRqp4Gz+Q1lVFzFZd/gElRLc4NDenICgNZCVCEBQhIdYhWwc4rM/1/Gag249PSLOXILyEAQg9aLS1jmyS9LKF1AbcnuE2b7/1r4iWtD1d97aj/zH+/SxCCHX0UUwgzA6kIvh90rLAwEEO1/idd2S9+TKqDpKt++lGUIIsFDieJdYZR+Phr1j4fS3xKg="
-#    git remote add originx https://"${tokenx}"@github.com/mdj33/plugin.git >/dev/null 2>&1
-#    git push --quiet --set-upstream originx $TRAVIS_BRANCH
-    git push --force --quiet "https://${tokenx}@github.com/mdj33/plugin.git" origin:$TRAVIS_BRANCH
+#    local tokenx="edvumnefzvrhdi+wHdE3FkKMghDC0n8IyA6pTU1b2o9COlGFmbDv7A2LNVPagrATAwh9iLWmh6VD7fErHHActOpwi5LkszXci+nM/YsyUgqibdteUCP97dxSILvS3LFI/6bHwWfjPcjYEuykPXLPt9db1i4O3lB4vnQ/nlekb36G+75tfXOXTLDZFgYGLXypQT1mKrnKTqRtmbRq001OQGkypur+mHZmrN4B2gEvbKc2kONdfGvxcp6uOMQyojpgaVDwEw8NKLMDFYWlU6FnYtmc7aiwyDfN8czzv3nicHEs9z2GHvk7l0rD0zE7kj2mxIES90qfc97zbppqxzDlJMMjnUDaQLdhAjyMxrWkjyTeMRRGUNzJqXa+LuiFjWwUlvQMAJttqwWBv832gT6ayCfFtINNwGkECI+lC4IKCt542JdG5ncfyI38Sy1YhMmjVE93trbvUCkd9jy30x3/5Wdqmuq/09gOObdLPrDYiqXPmwgeYRqp4Gz+Q1lVFzFZd/gElRLc4NDenICgNZCVCEBQhIdYhWwc4rM/1/Gag249PSLOXILyEAQg9aLS1jmyS9LKF1AbcnuE2b7/1r4iWtD1d97aj/zH+/SxCCHX0UUwgzA6kIvh90rLAwEEO1/idd2S9+TKqDpKt++lGUIIsFDieJdYZR+Phr1j4fS3xKg="
+    git remote add originx https://"${MDJ33_TOKEN}"@github.com/mdj33/plugin.git >/dev/null 2>&1
+    git push --quiet --set-upstream originx $TRAVIS_BRANCH
+#    git push --force --quiet "https://${tokenx}@github.com/mdj33/plugin.git" origin:$TRAVIS_BRANCH
 }
 
 if [ $TRAVIS_PULL_REQUEST == false ];then
