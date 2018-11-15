afe5cbb6983cb58605f205438dc8c24bfd4a87c0
mdj33 <mdj@disanbo.com>
2018-11-15 14:04:01 +0800

    test pr br-2

--- build/travis_auto_ci.sh
+++ build/travis_auto_ci.sh
@@ -2,6 +2,7 @@
 #
 # Code coverage generation
 #set -e -o pipefail
+set -v
 
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH"
@@ -12,14 +13,39 @@ echo "TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG"
 echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR"
 echo "cur dir"
 pwd
-basepath=$(cd `dirname $0`; pwd)
+basepath=$(
+    cd $(dirname $0)
+    pwd
+)
 echo "base=$basepath"
 echo "git branch"
 git branch
 echo "git status"
 git status
 
+echo "token=$MDJ33_TOKEN"
+git remote -v
+git branch -r
+setup_git() {
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+}
 
+commit_website_files() {
+#    git checkout -b gh-pages
+    git status
+    git add -u
+    git commit --message "Travis build"
+#    git push origin HEAD:$TRAVIS_BRANCH
+}
 
+upload_files() {
+    git remote add origin-pages https://${MDJ33_TOKEN}@https://github.com/mdj33/plugin.git >/dev/null 2>&1
+    git push --quiet --set-upstream origin-pages HEAD:$TRAVIS_BRANCH
+}
 
-
+if [ $TRAVIS_PULL_REQUEST == false ];then
+    setup_git
+    commit_website_files
+    upload_files
+fi
