229d17c7a6c482c401920c68fcf4ea4f430aec2b
mdj33 <travis@travis-ci.org>
2018-11-15 22:05:34 +0800

    test pr br-13

--- travis_auto_ci.sh
+++ travis_auto_ci.sh
@@ -31,7 +31,7 @@ git branch -r
 setup_git() {
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "mdj33"
-    git clone --quiet --branch=add_autoci https://${GH_TOKEN}@github.com/mdj33/plugin.git add_autoci
+#    git clone --quiet --branch=add_autoci https://${GH_TOKEN}@github.com/mdj33/plugin.git add_autoci
 }
 
 commit_website_files() {
@@ -60,10 +60,10 @@ upload_files() {
 #    git remote rm origin
 #    local tokenx="edvumnefzvrhdi+wHdE3FkKMghDC0n8IyA6pTU1b2o9COlGFmbDv7A2LNVPagrATAwh9iLWmh6VD7fErHHActOpwi5LkszXci+nM/YsyUgqibdteUCP97dxSILvS3LFI/6bHwWfjPcjYEuykPXLPt9db1i4O3lB4vnQ/nlekb36G+75tfXOXTLDZFgYGLXypQT1mKrnKTqRtmbRq001OQGkypur+mHZmrN4B2gEvbKc2kONdfGvxcp6uOMQyojpgaVDwEw8NKLMDFYWlU6FnYtmc7aiwyDfN8czzv3nicHEs9z2GHvk7l0rD0zE7kj2mxIES90qfc97zbppqxzDlJMMjnUDaQLdhAjyMxrWkjyTeMRRGUNzJqXa+LuiFjWwUlvQMAJttqwWBv832gT6ayCfFtINNwGkECI+lC4IKCt542JdG5ncfyI38Sy1YhMmjVE93trbvUCkd9jy30x3/5Wdqmuq/09gOObdLPrDYiqXPmwgeYRqp4Gz+Q1lVFzFZd/gElRLc4NDenICgNZCVCEBQhIdYhWwc4rM/1/Gag249PSLOXILyEAQg9aLS1jmyS9LKF1AbcnuE2b7/1r4iWtD1d97aj/zH+/SxCCHX0UUwgzA6kIvh90rLAwEEO1/idd2S9+TKqDpKt++lGUIIsFDieJdYZR+Phr1j4fS3xKg="
 
-#    git remote add originx https://${GH_TOKEN}@github.com/mdj33/plugin.git
+    git remote add originx https://${GH_TOKEN}@github.com/mdj33/plugin.git
     git remote -v
-#    git push --quiet --set-upstream origin gh-pages
-    git push -fq origin add_autoci
+    git push --quiet --set-upstream originx $TRAVIS_BRANCH
+#    git push -fq origin add_autoci
 #    git push --force --quiet "https://${tokenx}@github.com/mdj33/plugin.git" origin:$TRAVIS_BRANCH
 }
 
